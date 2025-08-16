a44626660b9b2825e4c5f904ea0d34abe2767736
Gerogie18 <jennlyver@gmail.com>
2025-08-15 23:32:01 -0230

    Update Home.jsx

--- binary-tree/src/pages/home/Home.jsx
+++ binary-tree/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import UserSelectionDiv from '../../components/user-select/UserSelectionDiv.jsx';
+import './home.css';
 
 const Home = () => {
   return (
-    <>
+    <div className="home-container">
       <h1>Welcome to the Home Page</h1>
-    </>
+      <UserSelectionDiv />
+    </div>
   );
 };
 
